6bb9a474be40376e416940e74c635c10f00b3323
Mutah3r <evil.programmer.stephen@gmail.com>
2023-12-11 18:03:58 +0600

    add task adding and filtering button on tasks page

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,10 +1,25 @@
+import PrimaryButton from "../components/PrimaryButton";
 import SearchBox from "../components/SearchBox";
+import { IoOptionsOutline } from "react-icons/io5";
+import { FaPlus } from "react-icons/fa6";
 
 const Tasks = () => {
   return (
     <div>
+      {/* Search box */}
       <div>
         <SearchBox />
+      </div>
+
+      {/* Task page actions */}
+      <div className="mt-5 flex justify-between items-center">
+        <h2 className="text-neutral-600 text-[24px] font-semibold tracking-wider">
+          Tasks
+        </h2>
+        <div className="flex gap-3 items-center">
+          <IoOptionsOutline className="text-neutral-700 text-[20px] cursor-pointer" />
+          <PrimaryButton title="New task" icon={<FaPlus />} />
+        </div>
       </div>
     </div>
   );
