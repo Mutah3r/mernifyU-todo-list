f7fedf699835d5ab565d2a1d46f9322df4c40d5b
Mutah3r <evil.programmer.stephen@gmail.com>
2023-12-14 13:25:23 +0600

    update axios base url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiURL = "http://localhost:5001";
+const apiURL = "https://mernifyu-task-manager-server-side.onrender.com";
 
 export const axiosOpen = axios.create({
   baseURL: apiURL,
